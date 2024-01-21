7b4eafc292dd257268accd6ef0d7433f27c189f8
johnlichtenstein <johnlichtenstein@gmail.com>
2024-01-21 13:53:41 -0500

    python example scripts

--- ugly.py
+++ ugly.py
@@ -1,3 +1,4 @@
 if __name__ == "__main__":
-  msgL = ["This message is fine but it's against the PEP8 80 character", "limit to put it all on one line."]
-  print("\n".join(msgL))
+    msgL = ["This message is OK but it's against the PEP8 80 character", 
+            "limit to put it all on one line."]
+    print("\n".join(msgL))
